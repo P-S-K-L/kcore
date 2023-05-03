cdea8db7737fcbe0060be210e183545413b49acc
nalleyer <nalleyer@gmail.com>
2023-05-03 16:38:34 +0800

    area basic

---
+++ kcore/area.rkt
@@ -0,0 +1,44 @@
+#lang racket/base
+(require racket/list)
+
+(module+ test
+  (require rackunit))
+
+;; 一个区域，内含有序的卡片列表，还有很多标签，例如'me
+(struct area (owner cards tags) #:mutable)
+
+;; 从区域最上面获取number张卡
+(define (draw-area area number)
+  (let* ([cards (area-cards area)]
+         [len (length cards)])
+    (if (< len number)
+        (error ("not enough card for move. expected: ~a. got: ~a"  len number))
+        (let ([drawed (take cards number)]
+              [left (drop cards number)])
+          (begin
+            (set-area-cards! area left)
+            drawed)))))
+
+
+;; 塞到顶端
+(define (push-to-top area cards)
+  (set-area-cards! area (append cards (area-cards area))))
+
+(define (push-to-bottom area cards)
+  (set-area-cards! area (append (area-cards area) cards)))
+
+;; 洗牌
+(define (shuffle area)
+  (set-area-cards! area (shuffle (area-cards area))))
+
+(module+ test
+  (define test-area (area 'me (list 1 2 3 4 5) '(yard)))
+  (define drawed (draw-area test-area 2))
+  (check-equal? drawed (list 1 2))
+  (check-equal? (area-cards test-area) (list 3 4 5))
+  (push-to-top test-area (list 6 7))
+  (check-equal? (area-cards test-area) (list 6 7 3 4 5))
+  (push-to-bottom test-area (list 233))
+  (check-equal? (area-cards test-area) (list 6 7 3 4 5 233)))
+
+
